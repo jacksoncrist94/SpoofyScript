7dd3c9c7d5cc6a3276c971997fa6d5548d01443a
jacksoncrist94 <jackson.crist94@gmail.com>
2023-01-20 13:17:29 -0500

    changes to turn into simple webapp

--- spoofy.py
+++ spoofy.py
@@ -1,48 +1,53 @@
 import random
 import sys
+import io
+from flask import Flask, send_file
 from PIL import Image, ImageDraw, ImageFont
 
-try:
-    outFileName = sys.argv[1] + '.png'
-except IndexError:
-    outFileName = input('What would you like to name your file?\n') + '.png'
+app = Flask(__name__)
 
-suffixList=["core","step","style","gaze","punk","wave"," metal"]
-prefixList=["post-","neo-","alt-","proto-"]
-wordList=["sad","fear","dread","crypt","grief","ghoul","creep","sorrow","dismal"]
+@app.route("/")
+def index():
+    suffixList=["core","step","style","gaze","punk","wave"," metal"]
+    prefixList=["post-","neo-","alt-","proto-"]
+    wordList=["sad","fear","dread","crypt","grief","ghoul","creep","sorrow","dismal"]
 
-cemetery='cemetery.jpg'
-forest='forest.jpg'
-subway='subway.jpg'
+    cemetery='cemetery.jpg'
+    forest='forest.jpg'
+    subway='subway.jpg'
 
-imageList=[cemetery,forest,subway]
-
-
-mode=random.randint(0,1)
-suffixIndex=random.randint(0,len(suffixList)-1)
-prefixIndex=random.randint(0,len(prefixList)-1)
-wordIndex=random.randint(0,len(wordList)-1)
-imageIndex=random.randint(0,len(imageList)-1)
-
-if mode == 0:
-    genre = wordList[wordIndex] + suffixList[suffixIndex]
-else:
-    genre = prefixList[prefixIndex] + wordList[wordIndex]
+    imageList=[cemetery,forest,subway]
 
 
-message = genre
-font = ImageFont.truetype('CircularStd-Medium.otf',size=80)
-img = Image.open(imageList[imageIndex], 'r')
-logo = Image.open(r"spoofy_logo.png")
+    mode=random.randint(0,1)
+    suffixIndex=random.randint(0,len(suffixList)-1)
+    prefixIndex=random.randint(0,len(prefixList)-1)
+    wordIndex=random.randint(0,len(wordList)-1)
+    imageIndex=random.randint(0,len(imageList)-1)
 
-box=(10,10)
+    if mode == 0:
+        genre = wordList[wordIndex] + suffixList[suffixIndex]
+    else:
+        genre = prefixList[prefixIndex] + wordList[wordIndex]
 
-img.paste(logo, box, logo)
 
-imgDraw = ImageDraw.Draw(img)
+    message = genre
+    font = ImageFont.truetype('CircularStd-Medium.otf',size=80)
+    img = Image.open(imageList[imageIndex], 'r')
+    logo = Image.open(r"spoofy_logo.png")
 
-imgDraw.text((10, 410), message, font=font, fill="white")
+    box=(10,10)
 
-img.save(outFileName)
+    img.paste(logo, box, logo)
 
-img.show()
+    imgDraw = ImageDraw.Draw(img)
+
+    imgDraw.text((10, 410), message, font=font, fill="white")
+
+    img_io = io.BytesIO()
+    img.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
