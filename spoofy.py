9c2471208d2a4e1ec0c2c150fb8a7aed98dc886e
jacksoncrist94 <jackson.crist94@gmail.com>
2023-01-20 01:53:53 -0500

    added capability for random images

--- spoofy.py
+++ spoofy.py
@@ -1,17 +1,22 @@
 import random
 from PIL import Image, ImageDraw, ImageFont
 
-width = 512
-height = 512
-
 suffixList=["core","step","style","gaze","punk","wave"]
 prefixList=["post-","neo-","alt-","proto-"]
 wordList=["sad","doom","death","crypt","grief","ghoul","creep","sorrow","dismal"]
+
+cemetery='cemetery.jpg'
+forest='forest.jpg'
+subway='subway.jpg'
+
+imageList=[cemetery,forest,subway]
+
 
 mode=random.randint(0,1)
 suffixIndex=random.randint(0,len(suffixList)-1)
 prefixIndex=random.randint(0,len(prefixList)-1)
 wordIndex=random.randint(0,len(wordList)-1)
+imageIndex=random.randint(0,len(imageList)-1)
 
 if mode == 0:
     genre = wordList[wordIndex] + suffixList[suffixIndex]
@@ -21,7 +26,7 @@ else:
 
 message = genre
 font = ImageFont.truetype('CircularStd-Medium.otf',size=80)
-img = Image.open(r"cemetery.jpg")
+img = Image.open(imageList[imageIndex], 'r')
 logo = Image.open(r"spoofy_logo.png")
 
 box=(10,10)
@@ -30,8 +35,8 @@ img.paste(logo, box, logo)
 
 imgDraw = ImageDraw.Draw(img)
 
-imgDraw.text((10, 410), message, font=font, fill=(255))
+imgDraw.text((10, 410), message, font=font, fill="white")
 
-img.save('result.png')
+img.save('example.png')
 
 img.show()
